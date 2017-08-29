01c93b37d6ed79f83d7f98a05b9330b6f3dd4eb7
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-08-29 10:57:52 -0700

    Add cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
 
 app.use(bodyParser.json({ type: '*/*' }))
 app.use(morgan('combined'))
+app.use(cors())
 app.use('/api', routes)
 
 const port = process.env.PORT || 8080
