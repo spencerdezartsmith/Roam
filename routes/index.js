b9cc68289ab9e7adf2f1c3b1d6cfd57c8af560af
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-08-29 13:14:51 -0700

    Add auth to protected routes

--- routes/index.js
+++ routes/index.js
@@ -9,21 +9,18 @@ const UsersController = require('../controllers/users')
 const requireAuth = passport.authenticate('jwt', { session: false })
 const requireSignin = passport.authenticate('local', { session: false })
 
-router.get('/', (req, res, next) => {
-  res.send('hello world')
-})
+router.get('/', CitiesController.getAllCities)
 
 router.post('/signup', AuthController.signup)
 router.post('/signin', requireSignin, AuthController.signin)
 
-router.put('/users/:userId', UsersController.editProfile)
+router.put('/users/:userId/edit', requireAuth, UsersController.editProfile)
 
-router.get('/users/:userId/posts', PostsController.getAllPostsForUser)
-router.get('/users/:userId/posts/:postId', PostsController.getOnePostForUser)
-router.get('/cities/:cityId/posts', PostsController.getAllPostsForCity)
-router.post('/cities/:cityId/posts', PostsController.addNewPost)
-router.delete('/users/:userId/posts/:postId', PostsController.removeOnePost)
-
-router.get('/cities', CitiesController.getAllCities)
+router.get('/users/:userId/posts', requireAuth, PostsController.getAllPostsForUser)
+router.get('/users/:userId/posts/:postId', requireAuth, PostsController.getOnePostForUser)
+router.get('/cities/:cityId/posts', requireAuth, PostsController.getAllPostsForCity)
+router.post('/cities/:cityId/posts/new', requireAuth, PostsController.addNewPost)
+router.put('/users/:userId/posts/:postId/edit', requireAuth, PostsController.editOnePost)
+router.delete('/users/:userId/posts/:postId', requireAuth, PostsController.removeOnePost)
 
 module.exports = router
